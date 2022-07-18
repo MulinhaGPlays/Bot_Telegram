269bf5631fb07f2b020263a1d53366bcb74a0bfb
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-18 18:26:25 -0300

    bot simples feito

--- venv/BotTelegram.py
+++ venv/BotTelegram.py
@@ -4,11 +4,31 @@ CHAVE_API = "5362897913:AAFkqmaFg3VJnRNYEB30yAT5EhCgGbpYNVY"
 
 bot = telebot.TeleBot(CHAVE_API)
 
-def verificar(mensagem): 
+############################################################################### Comandodos ###############################################################################
+
+@bot.message_handler(commands=['DizerBatata'])
+def responder(mensagem):
+    bot.reply_to(mensagem, "batata")
+    
+@bot.message_handler(commands=['DizerBatata'])
+def responder(mensagem):
+    bot.reply_to(mensagem, "tomate")
+        
+
+############################################################################### Verificadores ###############################################################################
+        
+def verificar(mensagem):
     return True
+
+############################################################################### Mensagens ###############################################################################
 
 @bot.message_handler(func=verificar)
 def responder(mensagem):
-    bot.reply_to(mensagem, "olá")
+    texto = """Escolha um Comando::
+    /DizerBatata
+    /DizerTomate"""
+    bot.reply_to(mensagem, texto)
+
+############################################################################### Analise de Texto ###############################################################################
 
 bot.polling()
