3bd82b27cfe175c8e5159ed0370f2f4c1fa2b665
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-19 12:41:33 -0300

    .

---
+++ Bots_Automação/Automação Excel/B1.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+tabela = pd.read_excel('Planilhas/tabela.xlsx')
+display(tabela)
--- Bots_Automação/Automações Gerais/B1.py
+++ Bots_Automação/Automações Gerais/B1.py
@@ -2,9 +2,9 @@ import pyautogui
 import time
 
 pyautogui.alert('Bem vindo ao B1, este BOT foi desenvolvido para automatizar algum processo desejado.')
+pyautogui.PAUSE = 1.25
 pyautogui.hotkey('winleft', 'r')
 pyautogui.write("opera.exe") 
 pyautogui.press('enter')
-time.sleep(1.2)
 pyautogui.write("https://www.github.com/MulinhaGPlays")
 pyautogui.press('enter')
