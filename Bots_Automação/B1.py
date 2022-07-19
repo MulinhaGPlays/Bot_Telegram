ca6e2e9643096e83b48c2fce890fa478d2404d54
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-19 12:07:46 -0300

    implementação do código de combinação de teclas

--- Bots_Automação/B1.py
+++ Bots_Automação/B1.py
@@ -2,9 +2,7 @@ import pyautogui
 import time
 
 pyautogui.alert('Bem vindo ao B1, este BOT foi desenvolvido para automatizar algum processo desejado.')
-pyautogui.keyDown('winleft')
-pyautogui.press('r')
-pyautogui.keyUp('winleft')
+pyautogui.hotkey('winleft', 'r')
 pyautogui.write("opera.exe") 
 pyautogui.press('enter')
 time.sleep(1.2)
