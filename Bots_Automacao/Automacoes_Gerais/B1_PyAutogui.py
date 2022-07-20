6765f254e860e3c3c56ba5b5630d4a3b3576087e
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-20 11:59:34 -0300

    mudança de delay

--- Bots_Automacao/Automacoes_Gerais/B1_PyAutogui.py
+++ Bots_Automacao/Automacoes_Gerais/B1_PyAutogui.py
@@ -2,9 +2,10 @@ import pyautogui
 import time
 
 pyautogui.alert('Bem vindo ao B1, este BOT foi desenvolvido para automatizar algum processo desejado.')
-pyautogui.PAUSE = 1.25
+pyautogui.PAUSE = 1
 pyautogui.hotkey('winleft', 'r')
 pyautogui.write("opera.exe") 
 pyautogui.press('enter')
+time.sleep(2)
 pyautogui.write("https://www.github.com/MulinhaGPlays")
 pyautogui.press('enter')
