3c5df92ab281bfd2c1908747f0549f1b7c42fb5a
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-25 11:12:37 -0300

    extração de dados

--- Bots_Blaze/BDouble.py
+++ Bots_Blaze/BDouble.py
@@ -1,39 +1,62 @@
+import time
+import telebot
 import regex as re
-
 from bs4 import BeautifulSoup
 
+from playwright.sync_api import sync_playwright
 
-HistoricResult = '<div class="entries main"><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">11</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">14</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box white"><img alt="0" src="/static/media/logo-icon.75d9365f.svg"></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">3</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">4</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box white"><img alt="0" src="/static/media/logo-icon.75d9365f.svg"></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">10</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">12</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">6</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">5</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">9</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">6</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">8</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">2</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">7</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">2</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">11</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">4</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">9</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">8</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">14</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">10</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">6</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box black"><div class="number">10</div></div></div></div><div class="entry"><div class="roulette-tile"><div class="sm-box red"><div class="number">4</div></div></div></div></div>'
-soup = BeautifulSoup(HistoricResult, 'html.parser')
-resultadosNumero = []
-resultadosCor = []
-ResultHistoric = []
+CHAVE_API = "5362897913:AAFkqmaFg3VJnRNYEB30yAT5EhCgGbpYNVY"
+chat_id = '1970700049'
 
-n = -1
-for resultados in soup.find_all('div', class_='sm-box'):
-    n += 1
-    resultadosCor.append(resultados['class'][1])
-    resultadosNumero.append(re.findall(r'\d+', resultados.text))
-    resultadosNumero[n] = resultadosNumero[n][0] if len(
-        resultadosNumero[n]) > 0 else '0'
-    ResultHistoric.append(f'{resultadosNumero[n]} {resultadosCor[n]}')
+bot = telebot.TeleBot(CHAVE_API)
 
-g = -1
-for f in ResultHistoric:
-    g += 1
-    print(ResultHistoric[g])
+def send_message(text):
+    bot.send_message(chat_id, text)
 
-# CHAVE_API = "5362897913:AAFkqmaFg3VJnRNYEB30yAT5EhCgGbpYNVY"
-# bot = telebot.TeleBot(CHAVE_API)
+# with sync_playwright() as playwright:
+#     resultadosNumero = []
+#     resultadosCor = []
+#     ResultHistoric = []
+#     chromium = playwright.chromium
+#     browser = chromium.launch()
+#     page = browser.new_page()
+#     page.goto('https://blaze.com/pt/games/double')
+#     HistoricResult = page.content()
+#     soup = BeautifulSoup(HistoricResult, 'html.parser')
+#     n = -1
+#     for resultados in soup.find_all('div', class_='sm-box'):
+#         n += 1
+#         resultadosCor.append(resultados['class'][1])
+#         resultadosNumero.append(re.findall(r'\d+', resultados.text))
+#         resultadosNumero[n] = resultadosNumero[n][0] if len(resultadosNumero[n]) > 0 else '0'
+#         ResultHistoric.append(f'{resultadosNumero[n]} {resultadosCor[n]}')
+#     g = -1
+#     for f in ResultHistoric:
+#         g += 1
+#         print(ResultHistoric[g])
+#         # send_message(ResultHistoric[g])
+#     browser.close()
 
-# def verificar(mensagem):
-#     return True
-
-
-# @bot.message_handler(func=verificar)
-# def responder(mensagem):
-#     texto = "ola"
-#     bot.reply_to(mensagem, texto)
-
-
-# bot.polling()
+with sync_playwright() as playwright:
+    resultadosNumero = []
+    resultadosCor = []
+    ResultHistoric = []
+    chromium = playwright.chromium
+    browser = chromium.launch()
+    page = browser.new_page()
+    page.goto('https://blaze.com/pt/games/double')
+    HistoricResult = page.content()
+    soup = BeautifulSoup(HistoricResult, 'html.parser')
+    n = -1
+    for resultados in soup.find_all('div', class_='lg-box'):
+        n += 1
+        resultadosCor.append(resultados['class'][1])
+        resultadosNumero.append(re.findall(r'\d+', resultados.text))
+        resultadosNumero[n] = resultadosNumero[n][0] if len(resultadosNumero[n]) > 0 else '0'
+        ResultHistoric.append(f'{resultadosNumero[n]} {resultadosCor[n]}')
+    g = -1
+    for f in ResultHistoric:
+        g += 1
+        print(ResultHistoric[g])
+        # send_message(ResultHistoric[g])
+    browser.close()
