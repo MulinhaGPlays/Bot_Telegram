5f3ace9de3d82ff0c11d2e26331782e621861d72
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-26 00:17:03 -0300

    requisição de histórico de resultados

--- Bots_Blaze/BDouble.py
+++ Bots_Blaze/BDouble.py
@@ -1,5 +1,6 @@
 import time
 import telebot
+import requests
 import regex as re
 from bs4 import BeautifulSoup
 
@@ -13,50 +14,89 @@ bot = telebot.TeleBot(CHAVE_API)
 def send_message(text):
     bot.send_message(chat_id, text)
 
-# with sync_playwright() as playwright:
-#     resultadosNumero = []
-#     resultadosCor = []
-#     ResultHistoric = []
-#     chromium = playwright.chromium
-#     browser = chromium.launch()
-#     page = browser.new_page()
-#     page.goto('https://blaze.com/pt/games/double')
-#     HistoricResult = page.content()
-#     soup = BeautifulSoup(HistoricResult, 'html.parser')
-#     n = -1
-#     for resultados in soup.find_all('div', class_='sm-box'):
-#         n += 1
-#         resultadosCor.append(resultados['class'][1])
-#         resultadosNumero.append(re.findall(r'\d+', resultados.text))
-#         resultadosNumero[n] = resultadosNumero[n][0] if len(resultadosNumero[n]) > 0 else '0'
-#         ResultHistoric.append(f'{resultadosNumero[n]} {resultadosCor[n]}')
-#     g = -1
-#     for f in ResultHistoric:
-#         g += 1
-#         print(ResultHistoric[g])
-#         # send_message(ResultHistoric[g])
-#     browser.close()
+def send_image(file_path):
+    body = {'chat_id': chat_id, }
+    files = {'photo': open(file_path, 'rb')}
+    r = requests.post(
+        'https://api.telegram.org/bot{}/sendPhoto'.format(CHAVE_API), data=body, files=files)
 
+
+send_message('Iniciando Bot...')
 with sync_playwright() as playwright:
-    resultadosNumero = []
-    resultadosCor = []
+    ResultNumber = []
+    ResultColor = []
     ResultHistoric = []
+    RouletteNumber = []
+    RouletteColor = []
+    RouletteQuant = []
     chromium = playwright.chromium
-    browser = chromium.launch()
+    browser = chromium.launch(headless=True)
     page = browser.new_page()
     page.goto('https://blaze.com/pt/games/double')
-    HistoricResult = page.content()
-    soup = BeautifulSoup(HistoricResult, 'html.parser')
-    n = -1
-    for resultados in soup.find_all('div', class_='lg-box'):
-        n += 1
-        resultadosCor.append(resultados['class'][1])
-        resultadosNumero.append(re.findall(r'\d+', resultados.text))
-        resultadosNumero[n] = resultadosNumero[n][0] if len(resultadosNumero[n]) > 0 else '0'
-        ResultHistoric.append(f'{resultadosNumero[n]} {resultadosCor[n]}')
-    g = -1
-    for f in ResultHistoric:
-        g += 1
-        print(ResultHistoric[g])
-        # send_message(ResultHistoric[g])
-    browser.close()
+    r = 0
+    send_message('Aguarde, estamos calculando os resultados...')
+    while True:
+        DataSite = page.content()
+        soup = BeautifulSoup(DataSite, 'html.parser')
+        n = -1
+        if r == 0:
+            for Div in soup.find_all('div', class_='sm-box'):
+                n += 1
+                ResultColor.append(Div['class'][1])
+                ResultNumber.append(re.findall(r'\d+', Div.text))
+                ResultNumber[n] = ResultNumber[n][0] if len(
+                    ResultNumber[n]) > 0 else '0'
+                ResultHistoric.append(
+                    f'{ResultNumber[n]} {ResultColor[n]}')
+            i = 0
+            for a in ResultHistoric:
+                i += 1
+            while i != 0:
+                if ResultColor[i-1] == 'red':
+                    for g in range(15):
+                        if f'{g}' == f'{ResultNumber[i-1]}':
+                            send_image(f'Bots_Blaze/Image/Red{g}.png')
+                if ResultColor[i-1] == 'black':
+                    for g in range(15):
+                        if f'{g}' == f'{ResultNumber[i-1]}':
+                            send_image(f'Bots_Blaze/Image/Black{g}.png')
+                if ResultColor[i-1] == 'white':
+                    send_image('Bots_Blaze/Image/White.png')
+                i = i-1
+            r = 1
+        elif r == 1:
+            Div2 = soup.find_all('div', id='roulette', class_='page complete')
+            Div1 = soup.find_all('div', class_='sm-box')[0]
+            if Div2 != []:
+                while Div2 == []:
+                    Div2 = soup.find_all('div', id='roulette', class_='page complete')
+                if Div2 != []:
+                    ResultNumberT = re.findall(r'\d+', Div1.text)
+                    ResultColorT = Div1['class'][1]
+                    ResultHistoricT = f'{ResultNumberT[0]} {ResultColorT}'
+                    if ResultColorT == 'red':
+                        for g in range(15):
+                            if f'{g}' == f'{ResultNumberT[0]}':
+                                send_image(f'Bots_Blaze/Image/Red{g}.png')
+                    if ResultColorT == 'black':
+                        for g in range(15):
+                            if f'{g}' == f'{ResultNumberT[0]}':
+                                send_image(f'Bots_Blaze/Image/Black{g}.png')
+                    if ResultColorT == 'white':
+                        send_image('Bots_Blaze/Image/White.png')
+                    time.sleep(3)
+        
+            # static
+        # n = -1
+        # for Div in soup.find_all('div', class_='lg-box'):
+        #     n += 1
+        #     RouletteColor.append(Div['class'][1])
+        #     RouletteNumber.append(re.findall(r'\d+', Div.text))
+        #     RouletteNumber[n] = RouletteNumber[n][0] if len(
+        #         RouletteNumber[n]) > 0 else '0'
+        #     RouletteQuant.append(f'{RouletteNumber[n]} {RouletteColor[n]}')
+        #     send_message(RouletteQuant[n])
+
+# outputs
+# RouletteQuant
+# ResultHistoric
--- README.md
+++ README.md
@@ -1,36 +1,20 @@
-1. Projetos Python
+# Projetos Python
 
 ### Bibliotecas Utilizadas:
-
 ###### - PyTelegramBotAPI
-
 ###### - Pandas
-
 ###### - Openpyxl
-
 ###### - PyAutogui
-
 ###### - Selenium
-
 ###### - WebDriver-Manager
-
 ###### - Certifi
-
 ###### - Colorama
-
 ###### - PySocks
-
 ###### - Requests
-
 ###### - Requests-Futures
-
 ###### - Stem
-
 ###### - TorreQuest
-
 ###### - Sherlock
-
 ###### - BeautifulSoup4
-
 ###### - Regex
 ###### - PlayWright
